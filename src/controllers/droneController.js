7ed472a2127115d4669fcad34a249aae57df0a29
Chathunka Tennakoon <Chathunka@users.noreply.github.com>
2023-06-03 12:48:03 +0530

    Development Stage:
    *update droneController for additional features (get state, set state, get battery level, set battery level, delete drone, get available drones for loading)
    *update droneRoutes for above mentioned methods

--- src/controllers/droneController.js
+++ src/controllers/droneController.js
@@ -48,3 +48,83 @@ exports.getAllDrones = (req, res) => {
         res.status(500).json({ error: 'Failed to retrieve drones' });
     });
 };
+
+exports.updateDroneState = (req, res) => {
+    const serialNumber = req.params.serialNumber;
+    const newState = req.body.state;
+
+    Drone.findOneAndUpdate({serialNumber}, {state:newState}, {new: true})
+    .then((drone) => {
+        if(!drone){
+            res.status(404).json({error: "Drone not found"});
+        }else{
+            res.status(200).json(drone);
+        }
+    })
+    .catch((err) => {
+        res.status(500).json({error: err});
+    });
+};
+
+// Delete a drone by serial number
+exports.deleteDrone = (req, res) => {
+    const serialNumber = req.params.serialNumber;
+  
+    Drone.findOneAndDelete({ serialNumber })
+    .then((drone) => {
+        if(!drone){
+            res.status(404).json({ error: 'Drone not found' });
+        }else{
+            res.status(200).json({ message: 'Drone deleted successfully' });
+        }
+    })
+    .catch((err) => {
+        res.status(500).json({ error: 'Failed to delete the drone' });
+    });
+};
+
+// Check the drone battery level by serial number
+exports.getDroneBatteryLevel = (req, res) => {
+    const serialNumber = req.params.serialNumber;
+  
+    Drone.findOne({ serialNumber })
+    .then((drone) => {
+        if(!drone){
+            res.status(404).json({ error: 'Drone not found' });
+        }else{
+            res.status(200).json({ batteryLevel: drone.batteryCapacity });
+        }
+    })
+    .catch((err) => {
+        res.status(500).json({ error: 'Failed to retrieve the drone battery level' });
+    });
+};
+
+// Update the battery level of a drone by serial number
+exports.updateDroneBatteryLevel = (req, res) => {
+    const serialNumber = req.params.serialNumber;
+    const newBatteryLevel = req.body.batteryCapacity;
+  
+    Drone.findOneAndUpdate({ serialNumber }, { batteryCapacity: newBatteryLevel }, { new: true })
+    .then((drone) => {
+        if(!drone){
+            res.status(404).json({ error: 'Drone not found' });
+        }else{
+            res.status(200).json(drone);
+        }
+    })
+    .catch((err) => {
+        res.status(500).json({ error: 'Failed to update the drone battery level' });
+    });
+};
+
+// Check available drones for loading
+exports.getAvailableDronesForLoading = (req, res) => {
+    Drone.find({ state: 'IDLE', batteryCapacity: { $gte: 25 } })
+    .then((drones) => {
+        res.status(200).json(drones);
+    })
+    .catch((err) => {
+        res.status(500).json({ error: 'Failed to retrieve available drones for loading' });
+    });
+};
--- src/routes/droneRoutes.js
+++ src/routes/droneRoutes.js
@@ -8,7 +8,22 @@ router.post('/', droneController.registerDrone);
 //Get all Drones
 router.get('/', droneController.getAllDrones);
 
+// Check available drones for loading
+router.get('/available-for-loading', droneController.getAvailableDronesForLoading);
+
 //Det Drone by Serial Number
 router.get('/:serialNumber', droneController.getDroneBySerialNumber);
+
+// Update the state of a drone
+router.put('/:serialNumber/state', droneController.updateDroneState);
+
+// Update the drone battery level for a given drone
+router.put('/:serialNumber/set-battery-level', droneController.updateDroneBatteryLevel);
+
+// Check the drone battery level for a given drone
+router.get('/:serialNumber/get-battery-level', droneController.getDroneBatteryLevel);
+
+// Delete a drone
+router.delete('/:serialNumber', droneController.deleteDrone);
 
 module.exports = router;
